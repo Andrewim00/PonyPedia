e17ab0a2191e79cd1849ee3489e4cbf2c4edab5d
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-05-31 15:06:39 -0300

    Create page.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function App(){
+    return(
+        <div>
+            <header>
+                <div>
+
+                </div>
+                <nav>
+                    <p>aaaaaaaaaaaaaaa</p>
+                </nav>
+            </header>
+
+            <section>
+                <h1>Land Page</h1>
+            </section>
+
+            <footer>
+
+            </footer>
+        </div>
+    );
+}
