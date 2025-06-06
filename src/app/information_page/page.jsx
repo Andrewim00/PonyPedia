4db4ef25791d02dcdee33061b05923268f2a94fa
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-06 14:29:06 -0300

    Update page.jsx

--- src/app/information_page/page.jsx
+++ src/app/information_page/page.jsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
 export default function informationPage(){
     return (
         <div>
-        
+            <header>
+                <Image 
+                src="/logo.png"
+                width={350}
+                height={350}
+                alt="Logo PoneyPédia"/>
+                <nav className="menu">
+                    <div>
+
+                    </div>
+                    <Link href={'/login'}>
+
+                    </Link>
+                </nav>
+            </header>
         </div>
     );
 }
