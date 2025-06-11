5c252e59ac8a534ad4365ede042d6e6075258412
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-10 22:00:46 -0300

    Update page.jsx

--- src/app/information_page/page.jsx
+++ src/app/information_page/page.jsx
@@ -1,27 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./page.module.css";
+import SiteHeader from "@/components/SiteHeader";
 
 
 export default function informationPage(){
     return (
         <div>
-            <header>
-                <div className={styles.logoContainer}>
-                    <Image
-                    className="logo"
-                    src='/logo.png'
-                    width={350}
-                    height={350}
-                    alt="logo PonyPédia"/>
-                </div>
-
-                <nav className="menu">
-                    <div></div>
-                    <Link href={'/login'}>Login</Link>
-                </nav>
-            </header>
-
+            <SiteHeader/>
+            
             <section></section>
 
             <footer></footer>
