c0addb8054f788813069c145d94dde7aafcdd88f
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-10 22:00:57 -0300

    Create SiteHeader.jsx

---
+++ src/components/SiteHeader.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SiteHeader(){
+    return(
+        <header>
+            <div className="logo-container">
+                <Image
+                className="logo "
+                alt="logo PonyPédia"
+                src='/logo.png'
+                height={350}
+                width={350}
+                layout="responsive"/>
+            </div>
+
+            <nav className="menu">
+                <div className="nav-links">
+                    <Link href={'/'} className="nav-item">Home</Link>
+                    <Link href={'/'} className="nav-item">O que somos?</Link>
+                    <Link href={'/'} className="nav-item">Sobre</Link>
+                </div>
+                <Link href={'/login'} className="botao-login">Login</Link>
+            </nav>
+        </header>
+    );
+}
