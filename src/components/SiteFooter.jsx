8c2a4006f7ede5534170998354af4d2794cce609
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-11 22:54:47 -0300

    Create SiteFooter.jsx

---
+++ src/components/SiteFooter.jsx
@@ -0,0 +1,9 @@
+export default function SiteFooter(){
+    return (
+        <footer>
+            <div className="footer-container">
+                <p className="footer-text">Feito com muito amor, carinho e magia 🌈!</p>
+            </div>
+        </footer>
+    );
+}
