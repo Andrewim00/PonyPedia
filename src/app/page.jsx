49d6132b76fd5177c5d7528fc8e16dfff7a14c1b
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-13 14:56:39 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import SiteHeader from "@/components/SiteHeader";
+import SiteFooter from "@/components/SiteFooter";
 
 
 export default function App(){
     return(
         <div>
-            <header>
-                <div>
-
-                </div>
-                <nav>
-                    <p>aaaaaaaaaaaaaaa</p>
-                </nav>
-            </header>
-
-            <section>
-                <h1>Land Page</h1>
-            </section>
-
-            <footer>
-
-            </footer>
+            <SiteHeader/>
         </div>
     );
 }
