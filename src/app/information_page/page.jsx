fa293484b51bd70e88ffe7e3c0900ab4e7f5f313
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-14 19:50:52 -0300

    Update page.jsx

--- src/app/information_page/page.jsx
+++ src/app/information_page/page.jsx
@@ -10,7 +10,14 @@ export default function informationPage(){
             
             <section>
                 <div className={styles.bodySiteContainer}>
+                    <main className={styles.mainContent}>
+                        <h1 className={styles.pageTitle}>Twilight Sparkle</h1>
+                        <div></div>
+                    </main>
 
+                    <aside className={styles.asideContent}>
+
+                    </aside>
                 </div>
             </section>
 
