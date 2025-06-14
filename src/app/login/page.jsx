e49e08b1391c9baaab79cb2768c4dfeabd8ad795
Matheus Betelli <mat23.betelli@gmail.com>
2025-06-13 22:47:01 -0300

    login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,24 @@
 
-export default function login() {
-    return(
-        <div></div>
-    );
+import styles from "./page.module.css";
+
+export default function Login() {
+  return (
+    <div className={styles.bodyLoginContainer}>
+      <section className={styles.loginBox}>
+        <h1 className={styles.title}>LOGIN</h1>
+
+        <form className={styles.form}>
+          <label className={styles.label}>Usuário</label>
+          <input type="text" className={styles.input} />
+
+          <label className={styles.label}>Senha</label>
+          <input type="password" className={styles.input} />
+
+          <button type="submit" className={styles.button}>Entrar</button>
+        </form>
+
+        <p className={styles.forgotPassword}>Esqueceu a senha?</p>
+      </section>
+    </div>
+  );
 }
