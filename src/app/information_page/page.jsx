dc067b3b5b3cea201f28667844bfc9b76cf39588
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-16 13:55:19 -0300

    Update page.jsx

--- src/app/information_page/page.jsx
+++ src/app/information_page/page.jsx
@@ -16,21 +16,15 @@ export default function informationPage(){
                     <main className={styles.mainContent}>
                         <h1 className={styles.pageTitle}>Twilight Sparkle</h1>
                         <p className={styles.paddingLine}></p>
-                        <p className={styles.paddingLine}></p>
                         <div className={styles.pageContainer}>
                             <div className={styles.content}>
                                 <h2 className={styles.subTitle}>Aparência</h2>
-                                <p className={styles.paddingLine}></p>
                                 <p className={styles.text}>Twilight Sparkle é um alicórnio roxo claro com cabelo azul-marinho e listras rosa e roxas. Suas pontas são retas e seu cabelo lembra um corte chanel. Ela tem olhos roxos e uma marca, que se tornou um símbolo de seu elemento da magia, uma estrela rosa de seis pontas com cinco estrelas brancas menores. </p>
                                 <p className={styles.paddingLine}></p>
-                                <p className={styles.paddingLine}></p>
                                 <h2 className={styles.subTitle}>História</h2>
-                                <p className={styles.paddingLine}></p>
                                 <p className={styles.text}>Twilight Sparkle vive em uma torre de marfim em Canterlot até que a Princesa Celestia a envia para Ponyville. Acompanhada por Spike, Twilight verifica os preparativos da cidade para a Celebração do Sol de Verão e conhece o resto dos personagens principais. A celebração é interrompida quando Nightmare Moon, o inimigo de Celestia, retorna ao poder e ameaça trazer a noite eterna para Equestria. Twilight e o resto das Mane Six descobrem os Elementos da Harmonia e os usam para transformar Nightmare Moon de volta na Princesa Luna. A Princesa Celestia dá permissão a Twilight para permanecer em Ponyville para estudar a magia da amizade.</p>
                                 <p className={styles.paddingLine}></p>
-                                <p className={styles.paddingLine}></p>
                                 <h2 className={styles.subTitle}>Cutie Mark</h2>
-                                <p className={styles.paddingLine}></p>
                                 <p className={styles.text}>Twilight conta como conseguiu sua cutie mark em "As Crônicas das Cutie Marks". Ela estudou magia com grande interesse quando era potro e tentou se matricular na Escola para Unicórnios Superdotados da Princesa Celestia, mas seu exame de admissão terminou em desastre quando ela não conseguiu chocar magicamente um ovo de dragão. Um momento após desistir, a onda de choque de uma explosão distante fez com que a magia de Twilight saísse do controle e causasse estragos na sala de exames. Depois que a Princesa Celestia apareceu e acalmou a situação, Twilight recobrou os sentidos e descobriu que sua cutie mark havia aparecido.</p>
                             </div>
                             <aside className={styles.asideContent}>
