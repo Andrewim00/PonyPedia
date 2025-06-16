f6c8f7a079e9cc0d0f398e59ae6b7cc2689c8ee7
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-16 14:33:11 -0300

    Update CharacterCard.jsx

--- src/app/home_page/components/CharacterCard.jsx
+++ src/app/home_page/components/CharacterCard.jsx
@@ -4,8 +4,23 @@ import styles from '../page.module.css';
 
 const CharacterCard = ({ name, imageUrl }) => {
   return (
-
-    <Link href={`/information_page?character=${name}`} className={styles.cardLink}>
+    <div>
+      <Link href={`/information_page?character=${name}`} className={styles.cardLink}>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={imageUrl}
+              alt={`Imagem de ${name}`}
+              width={150}
+              height={150}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.nameContainer}>
+            <p className={styles.name}>{name}</p>
+          </div>
+        </div>
+      </Link>
       <div className={styles.card}>
         <div className={styles.imageContainer}>
           <Image
@@ -19,20 +34,6 @@ const CharacterCard = ({ name, imageUrl }) => {
         <div className={styles.nameContainer}>
           <p className={styles.name}>{name}</p>
         </div>
-      </div>
-    </Link>
-    <div className={styles.card}>
-      <div className={styles.imageContainer}>
-        <Image
-          src={imageUrl}
-          alt={`Imagem de ${name}`}
-          width={150}
-          height={150}
-          className={styles.image}
-        />
-      </div>
-      <div className={styles.nameContainer}>
-        <p className={styles.name}>{name}</p>
       </div>
     </div>
   );
