16d02ee20300f707bb60cea792a015eb72578d58
Andreas <163583880+Andrewim00@users.noreply.github.com>
2025-06-16 13:55:27 -0300

    Update SiteHeader.jsx

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -16,9 +16,9 @@ export default function SiteHeader(){
 
             <nav className="menu">
                 <div className="nav-links">
-                    <Link href={'/'} className="nav-item">Home</Link>
-                    <Link href={'/'} className="nav-item">O que somos?</Link>
-                    <Link href={'/'} className="nav-item">Sobre</Link>
+                    <Link href={'/home_page'} className="nav-item">Home</Link>
+                    <Link href={'/home_page'} className="nav-item">O que somos?</Link>
+                    <Link href={'/home_page'} className="nav-item">Sobre</Link>
                 </div>
                 <Link href={'/login'} className="botao-login">Login</Link>
             </nav>
