d08234dfe5ae5d8df9f9623ff4d0464ac6de0e93
DanielDemico <135667661+DanielDemico@users.noreply.github.com>
2025-06-15 23:21:05 -0300

    LP

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import SiteHeader from "@/components/SiteHeader";
 import SiteFooter from "@/components/SiteFooter";
-
+import WelcomeSection from "./landing_page_components/welcome/welcome";
+import FindPlaceSection from "./landing_page_components/find_place/FindPlaceSection";
+import EmbarkSection from "./landing_page_components/embark/EmbarkSection";
+import "./globals.css";
 
 export default function App(){
     return(
         <div>
             <SiteHeader/>
+            <WelcomeSection />
+            <FindPlaceSection />
+            <EmbarkSection />
+            <SiteFooter />
         </div>
     );
 }
